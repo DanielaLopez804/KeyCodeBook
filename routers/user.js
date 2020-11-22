58d396dc55a0205b4c3f0458e61b32e77ba595c2
DanielaLopez804 <dl7241140@hotmail.com>
2020-11-22 15:11:12 -0500

    Correo

--- routers/user.js
+++ routers/user.js
@@ -8,7 +8,7 @@ module.exports=(app) => {
     app.get('/user/getOne/:id', user.getOne)
     app.delete('/user/deleteOne/:id',user.deleteOne)
     app.post('/login' , user.login)
-    app.get('/sendEmail', user.SendEmail)
+    app.get('/sendEmail', user.sendEmail)
 }
 
 
